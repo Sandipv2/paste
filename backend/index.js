7b0ad23acb337041c385d8e2354c1c951045c556
sandipv2 <sandiipvarma@gmail.com>
2025-07-12 17:20:43 +0530

    set static file

--- backend/index.js
+++ backend/index.js
@@ -3,24 +3,33 @@ import 'dotenv/config'
 import connectDB from "./config/mongodb.js";
 import cookieParser from "cookie-parser";
 import cors from 'cors';
+import path from 'path';
 
 import authRouter from "./routes/auth.route.js";
 import pasteRouter from './routes/paste.route.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+const __dirname = path.resolve();
 
 app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-    origin: [process.env.FRONTEND_URL, 'http://192.168.123.105:5173'],
+    origin: [process.env.FRONTEND_URL],
     credentials: true
 }))
 
 app.use('/api/auth', authRouter);
 app.use('/api/pastes', pasteRouter);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT, () => {
     connectDB();
